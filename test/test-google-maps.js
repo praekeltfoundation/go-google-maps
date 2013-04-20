204622c04ac706032307647b1040d6986daf3228
Simon de Haan <simon@praekeltfoundation.org>
2013-04-20 16:48:21 +0200

    tests pass for geolocation

--- test/test-google-maps.js
+++ test/test-google-maps.js
@@ -70,6 +70,39 @@ function check_state(user, content, next_state, expected_response, setup,
     maybe_call(teardown, this, [api, saved_user]);
 }
 
+function CustomTester(custom_setup, custom_teardown) {
+    var self = this;
+
+    self._combine_setup = function(custom_setup, orig_setup) {
+        var combined_setup = function (api) {
+            maybe_call(custom_setup, self, [api]);
+            maybe_call(orig_setup, this, [api]);
+        };
+        return combined_setup;
+    };
+
+    self._combine_teardown = function(custom_teardown, orig_teardown) {
+        var combined_teardown = function (api, saved_user) {
+            maybe_call(custom_teardown, self, [api, saved_user]);
+            maybe_call(orig_teardown, this, [api, saved_user]);
+        };
+        return combined_teardown;
+    };
+
+    self.check_state = function(user, content, next_state, expected_response,
+                                setup, teardown) {
+        return check_state(user, content, next_state, expected_response,
+                           self._combine_setup(custom_setup, setup),
+                           self._combine_teardown(custom_teardown, teardown));
+    };
+
+    self.check_close = function(user, next_state, setup, teardown) {
+        return check_close(user, next_state,
+                           self._combine_setup(custom_setup, setup),
+                           self._combine_teardown(custom_teardown, teardown));
+    };
+}
+
 describe("test_api", function() {
     it("should exist", function() {
         assert.ok(app.api);
@@ -83,18 +116,35 @@ describe("test_api", function() {
 });
 
 describe("test_google_maps", function() {
+
+    var fixtures = [
+        'test/fixtures/geolocation.json'
+    ];
+
+    var tester = new CustomTester(function (api) {
+        api.config_store.config = JSON.stringify({});
+        fixtures.forEach(function (f) {
+            api.load_http_fixture(f);
+        });
+    });
+
     it("new users should be asked where they are", function () {
-        check_state(null, null, "start_address",
+        tester.check_state(null, null, "start_address",
             "^Where are you now?");
     });
 
-    it("should return a list of geolocated matches", function() {
-        check_state({current_state: 'start_address'}, '9 Friend street',
-            'confirm_start_address', '^Please confirm');
+    it('should return a list of location matches', function() {
+        tester.check_state({current_state: 'start_address'},
+            '1600 Amphitheatre Parkway',
+            'confirm_start_address',
+            "^Select a match:[^]" +
+            "1. 1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA[^]" +
+            "2. None of the above$"
+            );
     });
 
-    it('should go to end when asked for them name', function() {
-        check_state({current_state: 'start_address'}, '9 Friend street',
-            'end', '^Thank you and bye bye!');
-    });
+    // it('should go to end when asked for them name', function() {
+    //     check_state({current_state: 'start_address'}, '9 Friend street',
+    //         'end', '^Thank you and bye bye!');
+    // });
 });
